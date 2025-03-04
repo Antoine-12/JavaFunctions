d52e79684c23b2110be1ed19897afdc1baadb210
Antoine-12 <aantoniolozano.14@outlook.com>
2025-03-04 00:43:37 -0600

    Add files via upload
    Java functions, f (x)

---
+++ JavaFunctions/src/javafunctions/JavaFunctions.java
@@ -0,0 +1,13 @@
+
+package javafunctions;
+
+public class JavaFunctions {
+
+    public static void main(String[] args) {
+        
+        Board show = new Board();
+        show.setVisible(true);
+        
+    }
+    
+}
