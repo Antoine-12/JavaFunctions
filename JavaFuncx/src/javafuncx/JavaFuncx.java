39210c0f6e3187b00807356d7341b506b43b989d
Antoine-12 <aantoniolozano.14@outlook.com>
2025-03-04 01:01:23 -0600

    Add files via upload
    Java Linux functions f de x

---
+++ JavaFuncx/src/javafuncx/JavaFuncx.java
@@ -0,0 +1,14 @@
+
+package javafuncx;
+
+
+public class JavaFuncx {
+    
+    public static void main(String[] args) {
+        
+        fdX shows = new fdX();
+        shows.setVisible(true);
+        
+    }
+    
+}
