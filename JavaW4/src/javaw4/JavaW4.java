9e32d7f89f73a9c23c54ccf01ee50411accf9734
Antoine-12 <aantoniolozano.14@outlook.com>
2025-03-04 22:56:57 -0600

    Add files via upload

---
+++ JavaW4/src/javaw4/JavaW4.java
@@ -0,0 +1,13 @@
+
+package javaw4;
+
+public class JavaW4 {
+
+    public static void main(String[] args) {
+      
+        Board2 show = new Board2();
+        show.setVisible(true);
+       
+    }
+    
+}
