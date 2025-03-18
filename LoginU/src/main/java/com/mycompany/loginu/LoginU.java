6d2ee5915e2fd2944c6f84ec6a80ed628df54b8e
Antoine-12 <aantoniolozano.14@outlook.com>
2025-03-17 23:33:56 -0600

    Add files via upload

---
+++ LoginU/src/main/java/com/mycompany/loginu/LoginU.java
@@ -0,0 +1,18 @@
+
+
+package com.mycompany.loginu;
+
+import java.util.ArrayList;
+
+public class LoginU {
+
+    public static ArrayList<User> user = new ArrayList(); 
+    
+    public static void main(String[] args) {
+        
+    toLogin sign = new toLogin();
+    sign.setVisible(true);
+    
+    
+    }
+}
---
+++ LoginU/src/main/java/com/mycompany/loginu/User.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.loginu;
+
+public class User {
+    
+    public String usr;
+    public String name;
+    public int role;
+    public String password;
+}
