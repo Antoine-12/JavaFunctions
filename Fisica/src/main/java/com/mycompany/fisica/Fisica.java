807c39f023c0c5d28779b5a5259d1b423abd4f53
Antoine-12 <aantoniolozano.14@outlook.com>
2025-05-06 11:11:02 -0600

    Add files via upload

---
+++ Fisica/src/main/java/com/mycompany/fisica/Fisica.java
@@ -0,0 +1,16 @@
+
+
+package com.mycompany.fisica;
+
+
+
+public class Fisica {
+    
+    public static void main(String[] args) {
+        
+        Menu mu = new Menu();
+        mu.setVisible(true);
+        
+        
+    }
+}
---
+++ Fisica/src/main/java/com/mycompany/fisica/point.java
@@ -0,0 +1,38 @@
+
+package com.mycompany.fisica;
+
+/**
+ *
+ * @author aanto
+ */
+public class point {
+    
+    private double x;
+    private double y;
+    private double finalSum;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getFinalSum() {
+        return finalSum;
+    }
+
+    public void setFinalSum(double finalSum) {
+        this.finalSum = finalSum;
+    }
+    
+}
